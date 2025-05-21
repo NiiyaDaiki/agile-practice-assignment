971d4b381b2bac0f7923e4ffb3ec3e0c9fd43d3a
NiiyaDaiki <56012694+NiiyaDaiki@users.noreply.github.com>
2025-05-22 00:26:31 +0900

    feat:googleログイン追加

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -22,9 +22,15 @@ export default function SignInPage() {
       <h1 className="text-3xl font-bold mb-6">サインイン</h1>
       <button
         onClick={() => signIn("github", { callbackUrl: "/" })}
-        className="px-6 py-3 bg-purple-700 text-white rounded-md"
+        className="px-6 py-3 m-2 bg-purple-700 text-white rounded-md cursor-pointer"
       >
         Githubでログイン
+      </button>
+      <button
+        onClick={() => signIn("google", { callbackUrl: "/" })}
+        className="px-6 py-3 m-2 bg-blue-500 text-white rounded-md cursor-pointer"
+      >
+        Googleでログイン
       </button>
     </div>
   );
